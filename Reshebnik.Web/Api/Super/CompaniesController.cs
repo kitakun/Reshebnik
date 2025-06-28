77af36c8d988c86428811eab80b512e49beab5ba
kitakun <xfikysx@ya.ru>
2025-06-28 22:19:12 +0700

    Add super companies controller

---
+++ Reshebnik.Web/Api/Super/CompaniesController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+using Reshebnik.Domain.Models;
+using Reshebnik.Handlers.Company;
+
+namespace Reshebnik.Web.Api.Super;
+
+[ApiController]
+[ApiExplorerSettings(GroupName = "Super")]
+[Route("api/super/[controller]")]
+public class CompaniesController : ControllerBase
+{
+    [HttpGet("typeahead")]
+    public async Task<IActionResult> TypeaheadAsync(
+        [FromQuery] TypeaheadRequest request,
+        [FromServices] SuTypeaheadCompaniesHandler handler,
+        CancellationToken cancellationToken)
+    {
+        var result = await handler.HandlerAsync(request, cancellationToken);
+        if (result == null) return Forbid();
+        return Ok(result);
+    }
+
+    [HttpGet("ids")]
+    public async Task<IActionResult> GetIdsAsync(
+        [FromServices] SuAllCompanyIdsHandler handler,
+        CancellationToken cancellationToken)
+    {
+        var result = await handler.HandleAsync(cancellationToken);
+        return Ok(result);
+    }
+}
