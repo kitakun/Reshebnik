752fd928d8b4a5a6bd92c141050f4e692def9a08
kitakun <xfikysx@ya.ru>
2025-07-17 22:29:34 +0700

    su user

---
+++ Reshebnik.EntityFramework/Migrations/20250717152914_SuUser2.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reshebnik.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class SuUser2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+migrationBuilder.Sql("""
+                                 INSERT INTO public."companies" (
+                                     "name",
+                                     "industry",
+                                     "employees_count",
+                                     "type",
+                                     "email",
+                                     "phone",
+                                     "notify_about_lowering_metrics",
+                                     "notification_type",
+                                     "language_code"
+                                 )
+                                 SELECT
+                                     'SuperCompany',
+                                     'IT',
+                                     2,
+                                     'Unset',
+                                     'info@supercompany.com',
+                                     '+1234567890',
+                                     true,
+                                     'Email',
+                                     'ru'
+                                 WHERE NOT EXISTS (
+                                     SELECT 1 FROM public."companies" WHERE "name" = 'SuperCompany'
+                                 );
+                                 """);
+            
+            migrationBuilder.Sql("""
+                             INSERT INTO public."employees" (
+                                 "Id", 
+                                 "company_id", 
+                                 "fio", 
+                                 "job_title", 
+                                 "email", 
+                                 "phone", 
+                                 "comment", 
+                                 "is_active", 
+                                 "email_invitation_code", 
+                                 "Password", 
+                                 "Salt", 
+                                 "role", 
+                                 "created_at", 
+                                 "last_login_at"
+                             )
+                             SELECT 
+                                 nextval('employee_id_seq'),
+                                 1,
+                                 'Илья',
+                                 'Team Lead',
+                                 'ya@kitakun.ru',
+                                 '+79999999999',
+                                 'Комментарий',
+                                 true,
+                                 null,
+                                 'K9cJw/bBtT3pHFO/yRXdb8tQk8LSLD271I7ODuquJ4Y=',
+                                 'PMRcoGKmOGsQCTzojJC+iw==',
+                                 'SuperAdmin', -- строковое значение RootRolesEnum
+                                 CURRENT_TIMESTAMP AT TIME ZONE 'UTC',
+                                 CURRENT_TIMESTAMP AT TIME ZONE 'UTC'
+                             WHERE NOT EXISTS (
+                                 SELECT 1 FROM public."employees" WHERE "email" = 'ya@kitakun.ru'
+                             );
+                             
+                             """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
