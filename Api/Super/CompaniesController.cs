7babd389cb4ff7eb95b3f24cc79b6df67537910a
kitakun <xfikysx@ya.ru>
2025-07-21 21:19:23 +0300

    su company switch method

--- Api/Super/CompaniesController.cs
+++ Api/Super/CompaniesController.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 using Reshebnik.Domain.Models;
 using Reshebnik.Domain.Models.Company;
+using Reshebnik.EntityFramework;
+using Reshebnik.Handlers.Auth;
 using Reshebnik.Handlers.Company;
+using Reshebnik.Web.Api.Client;
+using Reshebnik.Web.DTO.Auth;
+
+using System.Security.Claims;
 
 namespace Reshebnik.Web.Api.Super;
 
@@ -41,5 +48,25 @@ public class CompaniesController : ControllerBase
     {
         await handler.HandleAsync(request, cancellationToken);
         return Ok();
+    }
+
+
+    [HttpPost("switch")]
+    public async Task<IActionResult> SwitchAsync(
+        [FromQuery] int? companyId,
+        [FromServices] CreateJwtHandler handler,
+        [FromServices] IHttpContextAccessor httpContextAccessor,
+        [FromServices] ReshebnikContext dbContext,
+        CancellationToken cancellationToken
+    )
+    {
+        if (!companyId.HasValue) return BadRequest();
+        if (httpContextAccessor.HttpContext == null) return BadRequest();
+        var userId = httpContextAccessor.HttpContext.User.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier);
+        if (userId == null) return BadRequest();
+        var userIdInt = int.Parse(userId.Value);
+        var existingUser = await dbContext.Employees.AsNoTracking().Include(i => i.Company).FirstAsync(f => f.Id == userIdInt, cancellationToken);
+        var result = handler.CreateToken(existingUser, companyId);
+        return Ok(new AdminLoginResponse(result.User, result.Jwt, result.CurrentCompanyId));
     }
 }
